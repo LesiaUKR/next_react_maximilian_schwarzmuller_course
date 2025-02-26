ed585acac24804ac20ab881e2e8a8ac9ff2bef13
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-02-26 19:14:37 +0000

    feat(meal-page): add meal-grid and meal-items components and styles

--- section_03-foodies-project/app/meals/page.js
+++ section_03-foodies-project/app/meals/page.js
@@ -1,14 +1,25 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
+
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/recipe-1">Recipe 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/recipe-2">Recipe 2</Link>
-      </p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself! It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
