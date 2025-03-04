23541ec1307917a41fc38674af31c2324418dcc4
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-04 15:27:59 +0000

    feat(section-4): add parallel pages archive and latest

---
+++ section_04-routing-page-rendering-deep-dive/app/news/[newsSlug]/not-found.js
@@ -0,0 +1,11 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Page not found!</h1>
+      <p>
+        Unfortunately we couldn't find the news you were looking for. Please go
+        back to the news page.
+      </p>
+    </div>
+  );
+}
