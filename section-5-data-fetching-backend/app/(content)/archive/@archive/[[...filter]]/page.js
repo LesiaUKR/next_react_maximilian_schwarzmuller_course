62df08c6ae5a8d16b961938cfbdb9a2741bd0bf5
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-05 18:42:16 +0000

    feat(section-5):  granular data fetching with suspense

--- section-5-data-fetching-backend/app/(content)/archive/@archive/[[...filter]]/page.js
+++ section-5-data-fetching-backend/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,65 +1,20 @@
-import Link from "next/link";
-import NewsList from "@/components/news/news-list";
-import {
-  getAvailableNewsMonths,
-  getAvailableNewsYears,
-  getNewsForYear,
-  getNewsForYearAndMonth,
-} from "@/lib/news";
+import { Suspense } from "react";
+import FilteredNews from "@/components/news/filtered-news";
+import FilterHeader from "@/components/news/filter-header";
 
 export default async function FilteredNewsPage({ params }) {
   const filter = params.filter;
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
-  let news;
-  let links = await getAvailableNewsYears();
-
-  if (selectedYear && !selectedMonth) {
-    news = await getNewsForYear(selectedYear);
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links = [];
-  }
-
-  let newsContent = <p>No news found for the selected period.</p>;
-
-  if (news && news.length > 0) {
-    newsContent = <NewsList news={news} />;
-  }
-
-  const availableYears = await getAvailableNewsYears();
-
-  if (
-    (selectedYear && !availableYears.includes(selectedYear)) ||
-    (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error("Invalid filter");
-  }
   return (
     <>
-      <header id="archive-header">
-        <h1>News Archive</h1>
-        <nav>
-          <ul>
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-      {newsContent}
+      <Suspense fallback={<p>Loading filter ...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense>
+      <Suspense fallback={<p>Loading news ...</p>}>
+        <FilteredNews year={selectedYear} month={selectedMonth} />
+      </Suspense>
     </>
   );
 }
---
+++ section-5-data-fetching-backend/components/news/filter-header.js
@@ -0,0 +1,40 @@
+import { getAvailableNewsYears, getAvailableNewsMonths } from "@/lib/news";
+import Link from "next/link";
+
+export default async function FilterHeader({ year, month }) {
+  const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
+
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error("Invalid filter");
+  }
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+
+  return (
+    <header id="archive-header">
+      <h1>News Archive</h1>
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
