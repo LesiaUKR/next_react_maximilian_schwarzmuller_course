478aaca2c5f51bb74881210ff0a26785362ed332
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-11 20:14:16 +0000

    feat(section-12): add event details page, app layout, header, icons

--- section_12-file-based-project/components/events/event-item.js
+++ section_12-file-based-project/components/events/event-item.js
@@ -1,5 +1,8 @@
-import Link from 'next/link'
 import classes from './event-item.module.css'
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from './../icons/address-icon';
+import ArrowRightIcon from './../icons/arrow-right-icon';
 
 export default function EventItem(props) {
    const { title, image, location, id, date} = props;
@@ -16,14 +19,19 @@ export default function EventItem(props) {
          <div className={classes.summary} >
             <h2>{title}</h2>
             <div className={classes.date}>
+               <DateIcon />
                <time>{humanReadableDate}</time>
             </div>
             <div className={classes.address}>
+               <AddressIcon />
                <address>{formattedAddress}</address>
             </div>
          </div>
          <div className={classes.actions}>
-            <Link href={`/events/${id}`}>Explore Event</Link>
+            <Button link={`/events/${id}`}>
+               <span>Explore Event</span>
+               <span className={classes.icon}><ArrowRightIcon /></span>
+            </Button>
          </div>
       </div>
 
--- section_12-file-based-project/components/ui/button.js
+++ section_12-file-based-project/components/ui/button.js
@@ -3,7 +3,7 @@ import classes from "./button.module.css";
 
 export default function Button(props) {
   return (
-     <Link href={props.link} className={classes.btn}>
+     <Link href={props.link} className={classes.btn} >
       {props.children}
     </Link>
   )
