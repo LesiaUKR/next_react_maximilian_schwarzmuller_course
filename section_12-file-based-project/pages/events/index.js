03d905fb35bf90caa12a7f7092c2f5706bef7d80
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-11 20:18:10 +0000

    feat(section-12): add content to the all events page

--- section_12-file-based-project/pages/events/index.js
+++ section_12-file-based-project/pages/events/index.js
@@ -1,8 +1,12 @@
+import { getAllEvents } from "../../dummy-data"
+import EventList from './../../components/events/event-list';
 
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events Page</h1>
+     <EventList events={events} />
     </div>
   )
 }
