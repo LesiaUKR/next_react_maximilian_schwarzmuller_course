b4236e0c4378c78bafa4da8161078c0feec742ba
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-14 11:12:38 +0000

    feat(section-13): add example of useSWR as Client side data fetching

--- section_13-pre-rendering-data-fetching/pages/last-sales.js
+++ section_13-pre-rendering-data-fetching/pages/last-sales.js
@@ -1,35 +1,65 @@
 import { useEffect, useState } from "react"
+import useSWR from "swr";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function LastSalesPage() {
    const [sales, setSales] = useState();  
-   const [isLoading, setIsLoading] = useState(false);
+   // const [isLoading, setIsLoading] = useState(false);
 
+   const {data, error }=useSWR('https://nextjs-course-maxim-default-rtdb.europe-west1.firebasedatabase.app/sales.json',
+    fetcher);
 
-   useEffect(() => {
-      setIsLoading(true);
-      fetch('https://nextjs-course-maxim-default-rtdb.europe-west1.firebasedatabase.app/sales.json')
-         .then(response => response.json())
-         .then(data => {
-            const transformedSales = [];
-            for (const key in data) {
-               transformedSales.push({
-                  id: key,
-                  username: data[key].username,
-                  volume: data[key].volume
-               });
-            }
-            setSales(transformedSales);
-            setIsLoading(false);
-         });
-   }, []);
+   // useEffect(() => {
+   //    setIsLoading(true);
+   //    fetch('https://nextjs-course-maxim-default-rtdb.europe-west1.firebasedatabase.app/sales.json')
+   //       .then(response => response.json())
+   //       .then(data => {
+   //          const transformedSales = [];
+   //          for (const key in data) {
+   //             transformedSales.push({
+   //                id: key,
+   //                username: data[key].username,
+   //                volume: data[key].volume
+   //             });
+   //          }
+   //          setSales(transformedSales);
+   //          setIsLoading(false);
+   //       });
+   // }, []);
 
-   if (isLoading) {
+   // if (isLoading) {
+   //    return <p>Loading...</p>
+   // }
+
+   // if (!sales) {
+   //    return <p>No data yet</p>
+   // }
+
+   useEffect(() => { 
+      if(data){
+         const transformedSales = [];
+         for (const key in data) {
+            transformedSales.push({
+               id: key,
+               username: data[key].username,
+               volume: data[key].volume
+            });
+         }
+         setSales(transformedSales);
+      }
+   }, [data, error]);
+
+      if (error) {
+      return <p>Failed to load</p>
+   }
+
+   if (!data) {
       return <p>Loading...</p>
    }
 
-   if (!sales) {
-      return <p>No data yet</p>
+     if (!sales) {
+      return <p>Loading...</p>
    }
 
   return (
