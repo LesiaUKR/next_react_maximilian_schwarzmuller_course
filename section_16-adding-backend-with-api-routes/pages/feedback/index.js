b510b7039eda46220d3b2171af1382b62aa4f3f0
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-03-17 21:08:38 +0000

    feat(section-16): add example of the dynamic api routes [feedbackId]

--- section_16-adding-backend-with-api-routes/pages/feedback/index.js
+++ section_16-adding-backend-with-api-routes/pages/feedback/index.js
@@ -1,12 +1,25 @@
-import { buildFeedbackPath, extractFeedback } from '../api/feedback'
+import { Fragment, useState } from 'react';
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 export default function FeedbackPage(props) {
-  return (
-      <ul>
+   const [feedbackItem, setFeedbackItem] = useState(props.feedbackItems);
+   function feedbackDetailsHandler(id) { 
+fetch(`/api/${id}`)
+.then((response) => response.json())
+   .then((data) => { 
+      setFeedbackItem(data.feedback);
+ });
+   }
+   return (
+      <Fragment>
+         {feedbackItem && <p>{feedbackItem.email}</p>}
+         <ul>
         {props.feedbackItems.map((item) => (
-          <li key={item.id}>{item.feedback}</li>
+           <li key={item.id}>{item.feedback}
+           <button onClick={feedbackDetailsHandler.bind(null, item.id)}>Show Details</button>
+           </li>
         ))}
-      </ul>
+      </ul></Fragment> 
   );
 }
 
